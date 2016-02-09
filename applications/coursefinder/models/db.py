a037eeed212db4c13178514d71df1ad86062fe8f
joerozo <jrozo@ucsc.edu>
2016-02-08 17:25:43 -0800

    new comment

--- applications/coursefinder/models/db.py
+++ applications/coursefinder/models/db.py
@@ -14,6 +14,7 @@ from gluon.contrib.appconfig import AppConfig
 ## once in production, remove reload=True to gain full speed
 myconf = AppConfig(reload=True)
 
+#new comment
 
 if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
@@ -118,4 +119,3 @@ db.define_table('search',
 db.search.term.requires = IS_IN_SET(term)
 db.search.status.requires = IS_IN_SET(status)
 db.search.subject.requires = IS_IN_SET(subject)
-
